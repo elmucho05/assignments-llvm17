608e50e10f2a1dc8eee4f89d3f43e9a0aad8cbe5
elmucho05 <algertsuta@gmail.com>
2024-04-06 11:41:35 +0200

    added a few comments and little leggibility changes

--- assignment1/LocalOpts.cpp
+++ assignment1/LocalOpts.cpp
@@ -1,11 +1,3 @@
-//===-- LocalOpts.cpp - Example Transformations --------------------------===//
-//
-// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
-// See https://llvm.org/LICENSE.txt for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-//
-//===----------------------------------------------------------------------===//
-
 #include "llvm/Transforms/Utils/LocalOpts.h"
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instructions.h"
@@ -13,11 +5,25 @@
 
 using namespace llvm;
 
-// clang -emit-llvm -S -c file.c .o file.ll
-// opt
+bool algebraicIdentity(Instruction &I, Instruction::BinaryOps toDoOperation) {
+  int pos = 0;
+  for (auto operand = I.op_begin(); operand != I.op_end(); operand++, pos++) {
+    ConstantInt *costante = dyn_cast<ConstantInt>(operand);
+    if (costante) {
+      APInt value = costante->getValue();
+
+      if ((value.isZero() && toDoOperation == Instruction::Add) || (value.isOne() && toDoOperation == Instruction::Mul)) {
+        I.replaceAllUsesWith(I.getOperand(!pos));
+        outs() << "Trovata Algebraic Identity\n  nell'istruzione " << I << "\n  c'è un" << value << " in posizione " << pos<< "\n\n";
+        return true;
+      }
+    }
+  }
+  return false;
+}
 
 bool strengthReduction(Instruction &I, Instruction::BinaryOps toDoOperantion) {
-  int pos = 0;
+  int pos = 0;//per tenere conto della posizione degli operandi 
 
   for (auto operand = I.op_begin(); operand != I.op_end(); operand++, pos++) {
     ConstantInt *costante = dyn_cast<ConstantInt>(operand);
@@ -47,7 +53,7 @@ bool strengthReduction(Instruction &I, Instruction::BinaryOps toDoOperantion) {
   return false;
 }
 
-bool advStrengthReduction(Instruction &I) {
+bool advancedStrengthReduction(Instruction &I) {
   int pos = 0;
 
   for (auto operand = I.op_begin(); operand != I.op_end(); operand++, pos++) {
@@ -85,22 +91,6 @@ bool advStrengthReduction(Instruction &I) {
   return false;
 }
 
-bool algebraicIdentity(Instruction &I, Instruction::BinaryOps toDoOperation) {
-  int pos = 0;
-  for (auto operand = I.op_begin(); operand != I.op_end(); operand++, pos++) {
-    ConstantInt *costante = dyn_cast<ConstantInt>(operand);
-    if (costante) {
-      APInt value = costante->getValue();
-
-      if ((value.isZero() && toDoOperation == Instruction::Add) || (value.isOne() && toDoOperation == Instruction::Mul)) {
-        I.replaceAllUsesWith(I.getOperand(!pos));
-        outs() << "Trovata Algebraic Identity\n  nell'istruzione " << I << "\n  c'è un" << value << " in posizione " << pos<< "\n\n";
-        return true;
-      }
-    }
-  }
-  return false;
-}
 
 bool multiInstOpt(Instruction &I, Instruction::BinaryOps toDoOperation) {
   int pos = 0;
@@ -127,7 +117,6 @@ bool multiInstOpt(Instruction &I, Instruction::BinaryOps toDoOperation) {
           if (CUsee && opUsee->getOpcode() == opToFind && CUsee->getValue() == valueToFind){
             outs() << "Trovata Multi Instruction Optimization\n  " << I << " e " << *instUser << "\n ";
             instUser->replaceAllUsesWith(I.getOperand(!pos));
-
             return true;
           }
         }
@@ -148,8 +137,9 @@ bool runOnBasicBlock(BasicBlock &B) {
 
     switch (op->getOpcode()) {
     case BinaryOperator::Mul:
+      //si controlla prima se è una strength normale o una algebraic 
       if (!algebraicIdentity(inst, Instruction::Mul) && !strengthReduction(inst, Instruction::Mul))
-        advStrengthReduction(inst);
+        advancedStrengthReduction(inst);
       break;
     case BinaryOperator::Add:
       if (!algebraicIdentity(inst, Instruction::Add))
@@ -159,8 +149,6 @@ bool runOnBasicBlock(BasicBlock &B) {
     case BinaryOperator::Sub:
       multiInstOpt(inst, Instruction::Sub);
       break;
-
-    case (BinaryOperator::UDiv):
     case (BinaryOperator::SDiv):
       strengthReduction(inst, Instruction::SDiv);
       break;
